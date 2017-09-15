0efb33a2d1eb8b9397d52dfbf087e59dd5c8b877
Mnsour <mansour@ubuntu.ubuntu-domain>
2017-09-15 16:28:53 -0400

    Maybe done?

--- euler.c
+++ euler.c
@@ -1,31 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int smallest_mult(int test){
+int smallest_mult(){
   int number;
-  printf("%d \n",test);
-  for(number = 1; number < 21;number++){
-    if(test % number == 0){
+  int test;
+  for(test = 20; 1==1;test+= 20){
+    //printf("%d \n",test);
+    // Tests modulo 1-20
+    int works = 1;
 
+    for(number = 1; number < 21;number++){
+      //printf("Test: %d, Num: %d\n",test,number);
+      if(test % number != 0){
+	works = 0;
+	break;
+      }
     }
-    else{
-      return smallest_mult(test+1);
+      
+    if (works == 1){
+      return test;
     }
-
   }
-
-  return test;
-
+  return 0;
 
 }
 
-//int result = smallest_mult(1);
+int biggiePrime(){
+  int test;
+  int counter;
+  for(test = 2;counter < 10002;test++){
+
+    int isPrime = 1;
+    int i;
+    for(i=2;i<test;i++){
+      if(test%i == 0){
+	isPrime = 0;
+      }
+    }
+    
+    if(isPrime == 1){
+      counter++;
+    }
+
+
+    
+  }
+
+}
 
 
 
 
 
 int main(){
-  printf("Smallest multiple of all ints from 1-20 is %d",smallest_mult(1));
+  printf("The smallest multiple of all ints from 1-20 is %d \n",smallest_mult());
+  printf("The 10001st prime is %d \n", biggiePrime());
   return 0;
 }
